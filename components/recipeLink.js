2ad5bfaa40b309d878194e311742980f4d8bb7b6
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-10-05 14:11:42 -0700

    updated the recipe link format to address text overflow outside the card. now the intro fits neatly inside and all cards are same height

--- components/recipeLink.js
+++ components/recipeLink.js
@@ -5,14 +5,14 @@ import React from 'react'
 const RecipeLink = ({id,intro,title,url}) => {
   return (
     <Link href={`/recipes/${id}`}>
-    <div className='flex flex-col justify-around items-center w-96 mx-auto mt-6 p-3 shadow-2xl rounded-md hover:bg-black'>
+    <div className='flex flex-col justify-around items-center w-96 h-full mx-auto mt-6 p-3 shadow-2xl rounded-md hover:bg-black'>
     
         <div className='flex self-center p-2 h-52'>
             <Image className="rounded-md" src={url} width={200} height={200} alt={title}/>
         </div>
-        <div className='flex flex-col p-4 text-xl w-3/4 h-64 text-purple-700'>
+        <div className='flex flex-col p-4 text-xl w-3/4 h-80 text-purple-700'>
             <p className='text-2xl text-orange-600'>{title}</p>
-            <p>{intro}</p>
+            <p className='h-64'>{intro}</p>
         </div>
         
     </div>
